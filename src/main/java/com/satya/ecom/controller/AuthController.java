abbcdd1d502da99f23c18f324f2334b429982466
Satyabrata Sahoo <shivvchandan@gmail.com>
2025-04-19 18:46:48 +0530

    Updated response data

--- src/main/java/com/satya/ecom/controller/AuthController.java
+++ src/main/java/com/satya/ecom/controller/AuthController.java
@@ -9,19 +9,12 @@ import com.satya.ecom.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/auth")
